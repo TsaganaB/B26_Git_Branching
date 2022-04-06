9f1cc69bfb6ded371ed1a369aac31f1b480f39df
Galsan <Angira8080@gmail.com>
2022-04-06 19:40:37 -0400

    created the loop in fun

---
+++ src/test_cases/Problem.java
@@ -0,0 +1,10 @@
+package test_cases;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int i = 0; 5 < ; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
